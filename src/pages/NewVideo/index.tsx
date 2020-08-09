73eea9e632769bb26cfadb8fb9d75d744a7fc25d
CarlosETB <carloseduardotonholi@gmail.com>
2020-08-09 11:36:16 -0300

    feat(NewVideo): Created

---
+++ src/pages/NewVideo/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewVideo() {
+  return <div>Página de Cadastro de Video</div>;
+}
+
+export default NewVideo;
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,12 +5,14 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 // Pages
 import Home from "pages/Home";
+import NewVideo from "pages/NewVideo";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route component={Home} path="/" exact />
+        <Route component={NewVideo} path="/cadastro/video" exact />
         <Route component={() => <div>Página 404</div>} path="/" />
       </Switch>
     </BrowserRouter>
